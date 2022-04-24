d6a00930939da799d9b42ff7c030888e7e4894c4
Sampson Kotey Djanie <sampson.k.djanie@gmail.com>
2022-04-24 06:19:32 +0000

    Fetching Data from Backend

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Ghana: The Autobiography of Kwame Nkrumah',
+      slug: 'ghana-the-autobiography-of-kwame-nkrumah',
+      category: 'Shirts',
+      image: '/images/Nkrumah1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Ghana: The Autobiography of Kwame Nkrumah',
+      slug: 'ghana-the-autobiography-of-kwame-nkrumah',
+      category: 'Shirts',
+      image: '/images/Nkrumah1.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Ghana: The Autobiography of Kwame Nkrumah',
+      slug: 'ghana-the-autobiography-of-kwame-nkrumah',
+      category: 'Pants',
+      image: '/images/Nkrumah1.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Ghana: The Autobiography of Kwame Nkrumah',
+      slug: 'ghana-the-autobiography-of-kwame-nkrumah',
+      category: 'Pants',
+      image: '/images/Nkrumah1.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
